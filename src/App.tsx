011dc54176dc7e1608559b84e76decbf916128bd
bzhang98 <105823390+bzhang98@users.noreply.github.com>
2024-09-23 13:02:45 -0500

    remove terminal path

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Home from "./pages/home";
-import Terminal from "./pages/terminal";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/terminal" element={<Terminal />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
