2c90adad32f90fbe5d7530a7eb80c9b48c31b520
Brian Zhang <brianzhang234@gmail.com>
2025-06-16 16:30:44 -0500

    configure build for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        projects: resolve(__dirname, 'projects.html'),
+      }
+    }
+  }
 })
